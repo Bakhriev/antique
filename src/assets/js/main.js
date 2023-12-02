dd089198e8b722f92770d35e9efb724d0ae3f4fa
Murat <muratbakhriev@gmail.com>
2023-12-03 00:24:08 +0500

    faq

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,16 @@
 import {burgerMenu} from './functions/burger'
 
 burgerMenu()
+
+const accordions = document.querySelectorAll('.accordion')
+
+accordions.forEach(ac => {
+	const wrap = ac.querySelector('.accordion__wrapper')
+	const btn = ac.querySelector('.accordion__toggle')
+	btn.addEventListener('click', () => {
+		ac.classList.toggle('active')
+		wrap.style.maxHeight
+			? (wrap.style.maxHeight = '')
+			: (wrap.style.maxHeight = wrap.scrollHeight + 'px')
+	})
+})
